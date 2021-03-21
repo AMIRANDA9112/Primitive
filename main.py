4a6ff790b169e9b3330bd305082f2e09a35d204c
amiranda <Pipe91120707200>
2021-03-21 01:20:57 -0500

    mvp

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.graphics import Rectangle, Color, InstructionGroup
+from kivy.graphics import Rectangle, Color, InstructionGroup, Ellipse
 from kivy.clock import Clock
 import numpy as np
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
 
+from kivy.core.window import Window
 
-Config.set('graphics', 'width', 500)
-Config.set('graphics', 'height', 550)
+XP, YP = Window.width, Window.height
+
+Window.clearcolor = (((1 / 255) * 120), ((1 / 255) * 167), ((1 / 255) * 160), 1)
 
 XSIZE = 50
 YSIZE = 50
 
-dimCW = 500 / XSIZE
-dimCH = 500 / YSIZE
+dimCW = XP / 50
+dimCH = ((YP / 100) * 90) / 49
 
 F = 0
 
@@ -25,20 +27,20 @@ class Cell(Widget):
     def __init__(self, **kwargs):
         super(Cell, self).__init__(**kwargs)
         self.ig = InstructionGroup()
-        self.rect = Rectangle(size=(1, 1))
+        self.rect = Ellipse()
         self.color = Color(0, 1, 0)
         self.ig.add(self.color)
         self.ig.add(self.rect)
 
     def live_color(self):
         self.ig.clear()
-        self.color = Color(1, 0, 0)
+        self.color = Color(((1 / 255) * 47), ((1 / 255) * 51), ((1 / 255) * 25))
         self.ig.add(self.color)
         self.ig.add(self.rect)
 
     def death_color(self):
         self.ig.clear()
-        self.color = Color(0, 0, 1)
+        self.color = Color(((1 / 255) * 130), ((1 / 255) * 167), ((1 / 255) * 150))
         self.ig.add(self.color)
         self.ig.add(self.rect)
 
@@ -52,38 +54,34 @@ class MyGrid(GridLayout):
         super(MyGrid, self).__init__(**kwargs)
         self.cols = XSIZE
         self.rows = YSIZE
-        self.space = [1, 1]
-        self.col_force_default = True
-        self.row_force_default = True
-        self.col_default_width = 10
-        self.row_default_height = 10
 
         self.pause = True
-
         Clock.schedule_once(self.init_page)
 
     def init_page(self, dt):
 
-        cel = 0
-
         for y in range(YSIZE):
+
             for x in range(XSIZE):
+
                 self.add_widget(Cell())
+
                 if not self.game_state[x, y]:
 
                     self.children[0].death_color()
 
                     self.canvas.add(self.children[0].ig)
+
+
+
                 else:
 
                     self.children[0].live_color()
 
                     self.canvas.add(self.children[0].ig)
 
-                cel += 1
-
         Clock.schedule_once(self.set_attributes)
-        Clock.schedule_interval(self.init_game, 0.5)
+        Clock.schedule_interval(self.init_game, 0.1)
 
     def init_game(self, dt):
 
@@ -96,9 +94,11 @@ class MyGrid(GridLayout):
         for y in range(YSIZE):
             for x in range(XSIZE):
 
-                if not self.game_state[x, y]:
+                if self.game_state[x, y] == 0:
+
                     self.children[celg].death_color()
                     self.canvas.add(self.children[celg].ig)
+
                 else:
 
                     self.children[celg].live_color()
@@ -116,11 +116,12 @@ class MyGrid(GridLayout):
                             + self.game_state[(x - 1) % XSIZE, (y + 1) % YSIZE]
                             + self.game_state[x % XSIZE, (y + 1) % YSIZE]
                             + self.game_state[(x + 1) % XSIZE, (y + 1) % YSIZE]
-
                     )
 
                     if self.game_state[x, y] == 0 and n_neigh == 3:
                         self.new_game_state[x, y] = 1
+
+
 
                     elif self.game_state[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
                         self.new_game_state[x, y] = 0
@@ -139,10 +140,9 @@ class MyGrid(GridLayout):
     def on_touch_down(self, touch):
         posx, posy = touch.pos
 
-        if posy > 500:
+        if posy > (YP / 100) * 91:
             return
-
-        posx, posy = (49 - int(np.floor(posx / dimCW))), int(np.floor(posy / dimCH))
+        posx, posy = (49 - int(np.floor(posx / dimCW))), (int(np.floor(posy / dimCH)))
         self.game_state[posx, posy] = not self.new_game_state[posx, posy]
 
         print(posx, posy)
@@ -173,6 +173,7 @@ class MainBox(BoxLayout):
         self.grid_main = MyGrid()
         self.add_widget(self.box_buttons)
         self.add_widget(self.grid_main)
+
         print("main class active")
 
 
@@ -183,7 +184,6 @@ class Primitive(MainBox):
         self.box_buttons.start_button.bind(on_press=self.start_game)
         self.box_buttons.stop_button.bind(on_press=self.stop_game)
         self.box_buttons.clear_button.bind(on_press=self.clear_game)
-
         print("game init")
 
     def start_game(self, event):
@@ -195,7 +195,6 @@ class Primitive(MainBox):
     def clear_game(self, event):
         self.grid_main.game_state = np.zeros((XSIZE, YSIZE))
         self.grid_main.new_game_state = np.zeros((XSIZE, YSIZE))
-
         self.grid_main.pause = True
         print("m")
 
